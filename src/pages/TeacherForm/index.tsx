b477dcbe69b4e4aac97d07bc224f0f6d07403cc6
Jaqueline Fernandes <dhelly@gmail.com>
2020-08-05 11:10:02 -0300

    criação das rotas

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+function TeacherFrom() {
+  return (
+    <h1>TeacherFrom</h1>
+  );
+}
+
+export default TeacherFrom;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles.css';
+
+function TeacherList() {
+  return (
+    <h1>TeacherList</h1>
+  );
+}
+
+export default TeacherList;
